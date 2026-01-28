a8c809789a5b25c1f3e83264d77bbe50cae44deb
crowddevelopers08-arch <crowddevelopers08@gmail.com>
2026-01-28 17:42:33 +0530

    google add the thank page

--- app/thank-you/page.tsx
+++ app/thank-you/page.tsx
@@ -1,14 +1,24 @@
-import Navbars from '@/component/navbar-tk-pp'
-import ThankYouPage from '@/component/thank-you-lp'
-import React from 'react'
+import Navbars from "@/component/navbar-tk-pp";
+import ThankYouPage from "@/component/thank-you-lp";
+import Script from "next/script";
+import React from "react";
 
 const page = () => {
   return (
-    <div>
-      <Navbars />
+    <>
+      {/* Conversion Event Script */}
+      <Script id="conversion-event" strategy="afterInteractive">
+        {`
+          gtag('event', 'conversion', {'send_to': 'AW-17415937758/QRqeCJ082-0bEN69yPBA'});
+        `}
+      </Script>
+      
+      <div>
+        <Navbars />
         <ThankYouPage />
-    </div>
-  )
-}
+      </div>
+    </>
+  );
+};
 
-export default page
+export default page;
